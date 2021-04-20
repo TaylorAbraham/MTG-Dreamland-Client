f57afa9ab94bd492381609dfcdb92af4da64571e
Taylor Abraham <taylor.abra8@gmail.com>
2021-04-19 20:44:58 -0400

    Correct https URL

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
 export const SERVER_URL =
-  process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : 'http://mtg-dreamland-server.herokuapp.com/';
+  process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : 'https://mtg-dreamland-server.herokuapp.com/';
