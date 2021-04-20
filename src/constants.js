68b12dbde47011f7eea4d9d9e7448d26e42e903a
Taylor Abraham <taylor.abra8@gmail.com>
2021-04-19 20:00:58 -0400

    Fetch based on NODE_ENV

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,2 @@
-export const SERVER_URL = 'http://localhost:8080';
+export const SERVER_URL =
+  process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : 'http://mtg-dreamland-server.herokuapp.com/';
